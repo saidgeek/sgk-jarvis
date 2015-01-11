c3c08bdeb67e6a62251a397f48452ae52acfe3c6
Andres Espinace Sufan <saidgeek@MacBook-Pro-de-Andres.local>
2015-01-11 04:42:10 -0300

    Fix Symlink config

--- grunt.settings/symlink.js
+++ grunt.settings/symlink.js
@@ -3,27 +3,27 @@
 module.exports = {
 
 	gitconfig: {
-		dest: '<%= config.git.path_gitconfig %>',
-		relativeSrc: '<%= config.git.path_gitconfig_system %>'
+		relariveSrc: '<%= config.git.path_gitconfig %>',
+		dest: '<%= config.git.path_gitconfig_system %>'
 	},
 	gitignore: {
-		dest: '<%= config.git.path_gitignore %>',
-		relativeSrc: '<%= config.git.path_gitignore_system %>'
+		relativeSrc: '<%= config.git.path_gitignore %>',
+		dest: '<%= config.git.path_gitignore_system %>'
 	},
 	osx: {
-		dest: '<%= config.osx.path_osx %>',
-		relativeSrc: '<%= config.osx.path_osx_system %>'
+		relativeSrc: '<%= config.osx.path_osx %>',
+		dest: '<%= config.osx.path_osx_system %>'
 	},
 	zsh: {
-		dest: '<%= config.zsh.path_zshrc %>',
-		relativeSrc: '<%= config.zsh.path_zshrc_system %>'
+		relativeSrc: '<%= config.zsh.path_zshrc %>',
+		dest: '<%= config.zsh.path_zshrc_system %>'
 	},
 	sublime_install_packages: {
-		dest: '<%= config.sublime_text.path_sublime_install_packages %>',
-		relativeSrc: '<%= config.sublime_text.path_sublime_system %>'
+		relativeSrc: '<%= config.sublime_text.path_sublime_install_packages %>',
+		dest: '<%= config.sublime_text.path_sublime_system %>'
 	},
 	sublime_packages: {
-		dest: '<%= config.sublime_text.path_sublime_packages %>',
-		relativeSrc: '<%= config.sublime_text.path_sublime_system %>'
+		relativeSrc: '<%= config.sublime_text.path_sublime_packages %>',
+		dest: '<%= config.sublime_text.path_sublime_system %>'
 	}
 };
